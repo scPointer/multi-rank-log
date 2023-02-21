538d581f11dff97170da759179a4bff686f4feb9
yufeng <321353225@qq.com>
2023-02-22 02:15:28 +0800

    parse data and ref

--- src/main.ts
+++ src/main.ts
@@ -12,14 +12,19 @@ async function run(): Promise<void> {
   console.log(`stop-if-fail: ${stop_if_fail}`);
   console.log(`\n`);
 
-  let latestJsonFile = await readFile("latest.json");
+  let latestJsonFile = (await readFile("latest.json")).toString().trim();
+
+  // parse the json file.
+  let latestJson = {};
+  if (latestJsonFile.length > 0) latestJson = JSON.parse(latestJsonFile);
+
   let timed = new Date()
     .toISOString()
     .replace(/T/, "_")
     .substring(0, 19)
     .replace("-", "_")
     .replace(":", "_");
-  let ref = github.context.ref;
+  let ref = github.context.ref.split("/").pop()!;
   console.log(`timed ${timed}`);
   console.log(`ref ${ref}`);
   console.log(`latestJSONFile: ${latestJsonFile}`);
