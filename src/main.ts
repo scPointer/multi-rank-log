8469672eef3efb6e48cd414d57331186b8f93f1b
yufeng <321353225@qq.com>
2023-02-28 01:20:02 +0800

    update ci file

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,16 @@ async function run(): Promise<void> {
   let stop_if_fail = core.getInput("stop-if-fail") == "true";
 
   // read json file
-  let latestJsonFile = (await readFile("latest.json")).toString().trim();
+  let latestJsonFile;
+
+  try {
+    latestJsonFile = (await readFile("latest.json")).toString().trim()
+  } catch(error) {
+    latestJsonFile = "{}";
+  }
 
   // parse the json file.
-  let latestJson: any = {};
-  if (latestJsonFile.length > 0) latestJson = JSON.parse(latestJsonFile);
+  let latestJson: any = JSON.parse(latestJsonFile);
 
   // generate the time data
   let timed = new Date()
